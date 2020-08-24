9df023cd6b80deba78d811ff74b1cd32da674dcb
Martina Mucci <martina.m.mucci@pwc.com>
2020-08-24 14:28:38 -0300

    First Commit

---
+++ DesignPatterns/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace SingleResponsibilityPrinciple
+{
+    //  A class should have only one reason to change
+    //  It makes your software easier to implement and prevents unexpected 
+    // side-effects of future changes.
+
+    // just stores a couple of journal entries and ways of
+    // working with them
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\Users\mmucci001\Desktop\journal.txt";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
+        }
+    }
+}
