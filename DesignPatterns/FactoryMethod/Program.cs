7286cf59e977642cb799257dc185602384b9544d
Martina Mucci <martina.m.mucci@pwc.com>
2020-08-25 18:02:07 -0300

    Factory method

--- DesignPatterns/FactoryMethod/Program.cs
+++ DesignPatterns/FactoryMethod/Program.cs
@@ -18,36 +18,37 @@ namespace FactoryMethod
             Polar
         }
 
-        public static class PointFactory
-        {
-            //Factory method
-            public static Point NewCartesianPoint(double x, double y)
-            {
-                return new Point(x, y);
-            }
-
-            public static Point NewPolarPoint(double rho, double theta)
-            {
-                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-            }
-
-        }
+        
         public class Point
         {
             private double x, y;
 
-            public Point(double a, double b)
+            private Point(double a, double b)
             {
                 x = a;
                 y = b;
 
             }
 
+            public static class Factory
+            {
+                //Factory method
+                public static Point NewCartesianPoint(double x, double y)
+                {
+                    return new Point(x, y);
+                }
+
+                public static Point NewPolarPoint(double rho, double theta)
+                {
+                    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+                }
+
+            }
 
         }
         static void Main(string[] args)
         {
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
             WriteLine(point);
         }
     }
