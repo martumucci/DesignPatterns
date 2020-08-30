19627cdc560ba4298a2a955aa1d99d426f25bf8d
Martina Mucci <martina.m.mucci@pwc.com>
2020-08-30 20:37:01 -0300

    Composite

---
+++ DesignPatterns/Composite.Composite/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Composite.Composite
+{
+    //  It is a mechanism for treating individual objects and compositions of objects in a uniform manner.
+    //  IT lets you compose objects into tree structures and then work with these structures as if they 
+    // were individual objects.
+
+    //  Using this pattern only makes sense when the core model of your app can be represented as a tree.
+
+
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                .AppendLine(Name);
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "SQaure";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new GraphicObject { Name = "My drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color = "Blue" });
+            group.Children.Add(new Square { Color = "Blue" });
+            drawing.Children.Add(group);
+            WriteLine(drawing);
+        }
+    }
+}
