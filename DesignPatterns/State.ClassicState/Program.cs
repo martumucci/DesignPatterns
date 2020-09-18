48d7a30b418a9865ff2ab43f8efe7adcddc99daa
Martina Mucci <martina.m.mucci@pwc.com>
2020-09-18 11:46:45 -0300

    state

---
+++ DesignPatterns/State.ClassicState/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace State.ClassicState
+{
+    //  It is a pattern in which the object's behaviour is determined by
+    // its state. An object transitions from one state to another (something
+    // needs to trigger a transition).
+
+    //  A formalized construct which manages state and transitions is called a state machine.
+
+    //  Given sufficient complexity, it pays to formally define possible states and events/triggers
+
+    public class Switch
+    {
+        public State State = new OffState();
+        public void On() { State.On(this); }
+        public void Off() { State.Off(this); }
+    }
+
+    public abstract class State
+    {
+        public virtual void On(Switch sw)
+        {
+            Console.WriteLine("Light is already on.");
+        }
+        public virtual void Off(Switch sw)
+        {
+            Console.WriteLine("Light is already off.");
+        }
+    }
+
+    public class OffState : State
+    {
+        public OffState()
+        {
+            Console.WriteLine("Light turned off.");
+        }
+        public override void On(Switch sw)
+        {
+            Console.WriteLine("Turning light on... ");
+            sw.State = new OnState();
+        }
+    }
+
+    public class OnState : State
+    {
+        public OnState()
+        {
+            Console.WriteLine("Light turned on.");
+        }
+        public override void Off(Switch sw)
+        {
+            Console.WriteLine("Turning light off... ");
+            sw.State = new OffState();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ls = new Switch();
+            ls.On();
+            ls.Off();
+            ls.Off();
+        }
+    }
+}
---
+++ DesignPatterns/State.StateMachinesWithStateless/Program.cs
@@ -0,0 +1,42 @@
+﻿using Stateless;
+using System;
+using System.Web.Razor;
+
+namespace State.StateMachinesWithStateless
+{
+    public enum Health
+    {
+        NonReproductive,
+        Pregnant,
+        Reproductive
+    }
+
+    public enum Activity
+    {
+        GiveBirth,
+        ReachPuberty,
+        HaveAbortion,
+        HaveUnprotectedSex,
+        Historectomy
+    }
+
+    class Program
+    {
+        public static bool ParentsNotWatching { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var machine = new StateMachine<Health, Activity>(Health.NonReproductive);
+            machine.Configure(Health.NonReproductive)
+              .Permit(Activity.ReachPuberty, Health.Reproductive);
+            machine.Configure(Health.Reproductive)
+              .Permit(Activity.Historectomy, Health.NonReproductive)
+              .PermitIf(Activity.HaveUnprotectedSex, Health.Pregnant,
+                () => ParentsNotWatching);
+            machine.Configure(Health.Pregnant)
+              .Permit(Activity.GiveBirth, Health.Reproductive)
+              .Permit(Activity.HaveAbortion, Health.Reproductive);
+        }
+
+    }
+}
